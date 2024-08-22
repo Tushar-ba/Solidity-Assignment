c407f87f984792c072de2f8a4e2b964076350b88
Tushar-ba <tusharba321@gmail.com>
2024-08-22 16:53:36 +0530

    was successfull in getting the 2nd test case passing by fixing the liquidity minting and burning in MyTokenPair

--- contracts/MyDEX.sol
+++ contracts/MyDEX.sol
@@ -16,34 +16,47 @@ contract MyDEX {
     address pair = factory.getPair(tokenA, tokenB);
     require(pair != address(0), "Pair does not exist");
 
-    // Transfer tokens to the pair contract
-    IERC20(tokenA).transferFrom(msg.sender, pair, amountA);
-    IERC20(tokenB).transferFrom(msg.sender, pair, amountB);
+    require(IERC20(tokenA).transferFrom(msg.sender, address(this), amountA), "Transfer of tokenA failed");
+    require(IERC20(tokenB).transferFrom(msg.sender, address(this), amountB), "Transfer of tokenB failed");
 
-    // Call the mint function with the correct parameters
     MyTokenPair(pair).mint(msg.sender, amountA, amountB);
 }
 
     function swapTokens(address tokenIn, address tokenOut, uint amountIn) external {
-        address pair = factory.getPair(tokenIn, tokenOut);
-        require(pair != address(0), "Pair does not exist");
+    address pair = factory.getPair(tokenIn, tokenOut);
+    require(pair != address(0), "Pair does not exist");
 
-        IERC20(tokenIn).transferFrom(msg.sender, pair, amountIn);
-        
-        (uint reserveIn, uint reserveOut) = MyTokenPair(pair).getReserves();
-        uint amountOut = (amountIn * reserveOut) / reserveIn; // Simple swap logic
-        IERC20(tokenOut).transfer(msg.sender, amountOut);
+    MyTokenPair pairContract = MyTokenPair(pair);
+    (uint reserveIn, uint reserveOut) = pairContract.getReserves();
+
+    uint amountOut = (amountIn * reserveOut) / (reserveIn + amountIn); // Simple swap logic
+    
+    require(IERC20(tokenIn).transferFrom(msg.sender, address(this), amountIn), "Transfer of tokenIn failed");
+    require(IERC20(tokenOut).transfer(msg.sender, amountOut), "Transfer of tokenOut failed");
+
+    // Update reserves
+    if (tokenIn == pairContract.token0()) {
+        pairContract.updateReserves(reserveIn + amountIn, reserveOut - amountOut);
+    } else {
+        pairContract.updateReserves(reserveOut - amountOut, reserveIn + amountIn);
     }
+}
+// In MyDEX.sol
 
-    function removeLiquidity(address tokenA, address tokenB, uint liquidity) external {
-        address pair = factory.getPair(tokenA, tokenB);
-        require(pair != address(0), "Pair does not exist");
+function removeLiquidity(address tokenA, address tokenB, uint liquidity) external {
+    address pair = factory.getPair(tokenA, tokenB);
+    require(pair != address(0), "Pair does not exist");
 
-        // Call the burn function
-        (uint amountA, uint amountB) = MyTokenPair(pair).burn(msg.sender);
+    MyTokenPair pairContract = MyTokenPair(pair);
+    
+    // Transfer liquidity tokens from user to pair contract
+    require(pairContract.transferFrom(msg.sender, pair, liquidity), "Transfer failed");
 
-        // Transfer the amounts back to the user
-        IERC20(tokenA).transfer(msg.sender, amountA);
-        IERC20(tokenB).transfer(msg.sender, amountB);
-    }
+    // Call the burn function
+    (uint amountA, uint amountB) = pairContract.burn(msg.sender);
+
+    // Transfer the amounts back to the user
+    require(IERC20(tokenA).transfer(msg.sender, amountA), "Token A transfer failed");
+    require(IERC20(tokenB).transfer(msg.sender, amountB), "Token B transfer failed");
+}
 }
--- contracts/MyTokenPair.sol
+++ contracts/MyTokenPair.sol
@@ -3,55 +3,82 @@ pragma solidity ^0.8.19;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract MyTokenPair {
+contract MyTokenPair is IERC20 {
     address public token0;
     address public token1;
     uint public reserve0;
     uint public reserve1;
     mapping(address => uint) public balanceOf;
     mapping(address => mapping(address => uint)) public allowance;
+    uint private _totalSupply;
 
     constructor(address _token0, address _token1) {
         token0 = _token0;
         token1 = _token1;
     }
 
-    function mint(address to, uint amount0, uint amount1) external {
-    require(amount0 > 0 && amount1 > 0, "Invalid liquidity amounts");
-    reserve0 += amount0; // Update reserve for token0
-    reserve1 += amount1; // Update reserve for token1
-    balanceOf[to] += amount0 + amount1; // Mint liquidity tokens
+    function totalSupply() external view override returns (uint256) {
+        return _totalSupply;
     }
+
+    function transfer(address to, uint256 amount) external override returns (bool) {
+        return _transfer(msg.sender, to, amount);
+    }
+
+    function approve(address spender, uint256 amount) external override returns (bool) {
+        allowance[msg.sender][spender] = amount;
+        emit Approval(msg.sender, spender, amount);
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint256 amount) external override returns (bool) {
+        require(allowance[from][msg.sender] >= amount, "ERC20: insufficient allowance");
+        allowance[from][msg.sender] -= amount;
+        return _transfer(from, to, amount);
+    }
+
+    function _transfer(address from, address to, uint256 amount) internal returns (bool) {
+        require(balanceOf[from] >= amount, "ERC20: transfer amount exceeds balance");
+        balanceOf[from] -= amount;
+        balanceOf[to] += amount;
+        emit Transfer(from, to, amount);
+        return true;
+    }
+
+    function mint(address to, uint amount0, uint amount1) external {
+        require(amount0 > 0 && amount1 > 0, "Invalid liquidity amounts");
+        uint liquidity = sqrt(amount0 * amount1);
+        require(liquidity > 0, "Insufficient liquidity minted");
+
+        reserve0 += amount0;
+        reserve1 += amount1;
+        balanceOf[to] += liquidity;
+        _totalSupply += liquidity;
+        emit Transfer(address(0), to, liquidity);
+    }
+
     function burn(address to) external returns (uint amount0, uint amount1) {
         uint liquidity = balanceOf[to];
         require(liquidity > 0, "No liquidity to burn");
         
-        amount0 = liquidity * reserve0 / totalSupply();
-        amount1 = liquidity * reserve1 / totalSupply();
+        amount0 = liquidity * reserve0 / _totalSupply;
+        amount1 = liquidity * reserve1 / _totalSupply;
 
-        balanceOf[to] = 0;
+        balanceOf[to] -= liquidity;
+        _totalSupply -= liquidity;
         reserve0 -= amount0;
         reserve1 -= amount1;
+
+        emit Transfer(to, address(0), liquidity);
     }
 
     function getReserves() external view returns (uint, uint) {
         return (reserve0, reserve1);
     }
 
-    function approve(address spender, uint amount) external returns (bool) {
-        allowance[msg.sender][spender] = amount;
-        return true;
-    }
-
-    function transfer(address to, uint amount) external returns (bool) {
-        require(balanceOf[msg.sender] >= amount, "Insufficient balance");
-        balanceOf[msg.sender] -= amount;
-        balanceOf[to] += amount;
-        return true;
-    }
-
-    function totalSupply() public view returns (uint) {
-        return reserve0 + reserve1;
+    function updateReserves(uint _reserve0, uint _reserve1) external {
+        reserve0 = _reserve0;
+        reserve1 = _reserve1;
     }
 
     function sqrt(uint x) internal pure returns (uint y) {
