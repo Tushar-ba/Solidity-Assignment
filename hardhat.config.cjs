48104b2522108810ee0da62a8c7f5e357071c232
Tushar-ba <tusharba321@gmail.com>
2024-08-23 11:11:37 +0530

    fixed the deployment script and deployed it to the polygon network

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -1,9 +1,10 @@
 require('@nomiclabs/hardhat-ethers');
-require("@nomiclabs/hardhat-waffle");
+require('@nomiclabs/hardhat-waffle');
+require('dotenv').config();
 
 module.exports = {
     solidity: {
-        version: "0.8.20",
+        version: '0.8.20',
         settings: {
             optimizer: {
                 enabled: true,
@@ -13,7 +14,16 @@ module.exports = {
     },
     networks: {
         hardhat: {
-            chainId: 1337,
+            chainId: 1337,  // Default Hardhat network
+        },
+        localhost: {
+            url: 'http://127.0.0.1:8545',  // Localhost Hardhat network
+            chainId: 1337,  // Ensure this matches the Hardhat chainId
+        },
+        mumbai: {
+            url: `https://polygon-amoy.g.alchemy.com/v2/${process.env.POLYGON_API_KEY}`,
+            accounts: [`0x${process.env.PRIVATE_KEY}`],
+            chainId: 80002,
         },
     },
 };
